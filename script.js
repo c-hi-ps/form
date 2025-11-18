f1d9e78a3e09e022aacf4decf67c1505bdb730b3
c-hi-ps <shahdyusufuni@gmail.com>
2025-11-18 12:52:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.getElementById("contactForm").onsubmit = function () {
-  alert("Your order has been submitted. ");
-  return false; 
+document.getElementById("contactForm").onsubmit = function() {
+    alert("Your inquiry has been submitted.");
+    return false;
+};
+
+document.getElementsByTagName("tr")[1].onclick = function() {
+    alert("Amsterdam Chips Available!");
+};
+
+document.getElementsByTagName("tr")[2].onclick = function() {
+    alert("Regular Chips Available!");
 };
